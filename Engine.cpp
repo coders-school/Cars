f187a781dc9e53f3758be9db1cce2b405ba64965
Adam Mierzwiak <adam.mierzwiak@gmail.com>
2019-07-24 00:06:33 +0200

    Fix CMake for gtest & gmock

---
+++ Engine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adi on 23.07.19.
+//
+
+#include "Engine.h"
---
+++ Engine.hpp
@@ -0,0 +1,14 @@
+//
+// Created by adi on 23.07.19.
+//
+
+#ifndef CARS_ENGINE_H
+#define CARS_ENGINE_H
+
+
+class Engine {
+
+};
+
+
+#endif //CARS_ENGINE_H
