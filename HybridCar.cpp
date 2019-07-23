1e2db6d1d0c0e540628f29c6098d692f28ec340e
Szymon Gajewski <szymongaju@gmail.com>
2019-07-23 20:50:04 +0200

    Destructors for engine--> resolve problem with memory leak

--- HybridCar.cpp
+++ HybridCar.cpp
@@ -8,7 +8,12 @@ HybridCar::HybridCar(PetrolEngine* petrolEng, ElectricEngine* electricEng)
     std::cout << __FUNCTION__ << std::endl;
 }
 
-HybridCar::~HybridCar()       { std::cout << __FUNCTION__ << std::endl; }
+HybridCar::~HybridCar()
+{
+    delete ElectricCar::engine_;
+    delete PetrolCar::engine_;
+    std::cout << __FUNCTION__ << std::endl;
+}
 void HybridCar::feed() {
     PetrolCar::feed();
     ElectricCar::feed();
