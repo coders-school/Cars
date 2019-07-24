373d92dc371c63b481a9d18217cb9dec2a556572
piotrkowandy <piotr.kowandy@o2.pl>
2019-07-24 21:53:58 +0200

    Exception added

--- ElectricCar.cpp
+++ ElectricCar.cpp
@@ -25,6 +25,7 @@ ElectricEngine* ElectricCar::changeEngine (int power, int batteryCapacity)
     {
         if(velocity!=0)  throw ExceptionChangeEngine();
             std::cout << __FUNCTION__ << std::endl;
+	    delete engine_
             this->engine_ = new ElectricEngine(power,batteryCapacity);
             std::cout << "Power: "<<this->engine_->power_<<" Battery: "<<this->engine_->batteryCapacity_<< std::endl;
             return this->engine_;
@@ -32,14 +33,6 @@ ElectricEngine* ElectricCar::changeEngine (int power, int batteryCapacity)
    }
     catch (ExceptionChangeEngine Ex)
     {
-<<<<<<< HEAD
         Ex.getException();
-=======
-        std::cout << __FUNCTION__ << std::endl;
-	delete engine_;
-        this->engine_ = new ElectricEngine(power,batteryCapacity);
-        std::cout << "Power: "<<this->engine_->power_<<" Battery: "<<this->engine_->batteryCapacity_<< std::endl;
-        return this->engine_;
->>>>>>> 0aa3c19229b971bcb555edc087f65687024d9a48
     }
 }
