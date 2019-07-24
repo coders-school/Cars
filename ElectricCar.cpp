80adcc409e4d962f37859aa56fb45acf18d13000
piotrkowandy <piotr.kowandy@o2.pl>
2019-07-24 21:56:58 +0200

    ElectriCar updated

--- ElectricCar.cpp
+++ ElectricCar.cpp
@@ -25,7 +25,7 @@ ElectricEngine* ElectricCar::changeEngine (int power, int batteryCapacity)
     {
         if(velocity!=0)  throw ExceptionChangeEngine();
             std::cout << __FUNCTION__ << std::endl;
-	    delete engine_
+	    delete engine_;
             this->engine_ = new ElectricEngine(power,batteryCapacity);
             std::cout << "Power: "<<this->engine_->power_<<" Battery: "<<this->engine_->batteryCapacity_<< std::endl;
             return this->engine_;
