1d2d27a1154317a143bfe161f71309b121b4f372
Piotr Hoffner <piotr.hoffner.wroc@gmail.com>
2019-07-25 23:41:20 +0200

    fixed memory leaks

--- PetrolCar.cpp
+++ PetrolCar.cpp
@@ -7,7 +7,11 @@ PetrolCar::PetrolCar(PetrolEngine* engine)
     std::cout << __FUNCTION__ << std::endl;
 }
 
-PetrolCar::~PetrolCar()         { std::cout << __FUNCTION__ << std::endl; }
+PetrolCar::~PetrolCar()
+{
+    std::cout << __FUNCTION__ << std::endl;
+    delete engine_; 
+}
 void PetrolCar::refuel()        { std::cout << __FUNCTION__ << std::endl; }
 void PetrolCar::feed()          
 {
