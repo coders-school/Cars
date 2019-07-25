a2bc714f5ae67c04a2ba4c3b3b4d9200376c3c82
TheDude-cpu <dudek.u.sylwia@gmail.com>
2019-07-25 23:23:12 +0200

    changes for velocity

--- main.cpp
+++ main.cpp
@@ -9,16 +9,17 @@ int main()
     std::cout << std::endl << "OPEL" << std::endl;
     PetrolCar opel(new PetrolEngine(120, 1800, 6));
     car = &opel;
-    car->accelerate(50);
-    car->brake();
-    car->accelerate(-900);
+    car->Accelerate(50);
+    car->Brake(10);
+    car->Accelerate(-900);
     car->feed();
+    car->Velocity(35.5);
 
     std::cout << std::endl << "NISSAN" << std::endl;
     ElectricCar nissan(new ElectricEngine(130, 650));
     car = &nissan;
     car->feed();
-    car->accelerate(80);
+    car->Accelerate(80);
     //nissan.engine_= new ElectricEngine(150, 700);  // Changing an engine during driving is not safe
     car->turnLeft();
 
@@ -26,7 +27,7 @@ int main()
     auto* pe = new PetrolEngine(80, 1400, 5);
     HybridCar toyota(pe, new ElectricEngine(100, 540));
     car = &toyota;
-    car->accelerate(100);
-    car->brake();
+    car->Accelerate(100);
+    car->Brake(10);
     car->feed();
 }
