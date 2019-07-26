b8e02525d7decb97e4e148e622c02a0b47ef1c3f
Rafał <rafal.modzelewski@yahoo.pl>
2019-07-26 21:34:12 +0200

    updates

--- Car.hpp
+++ Car.hpp
@@ -2,14 +2,16 @@
 
 class Car
 {
+
 public:
-    int velocity =0;
     int Vmax;
+    int velocity = 0;
     virtual ~Car() = default;
     void turnLeft();
     void turnRight();
-    void brake(int time);
+    void brake(int breakTime);
     void accelerate(int speed);
     virtual void feed()=0;
+    
 };
 
