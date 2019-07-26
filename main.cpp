6d86b3382500fa43cfaac4b32b3317198c42b569
Piotr Hoffner <piotr.hoffner.wroc@gmail.com>
2019-07-26 16:23:45 +0200

    testing engine changing in HybridCar class objects

--- main.cpp
+++ main.cpp
@@ -8,11 +8,18 @@ int main()
 {
     PetrolCar fordMustang(new PetrolEngine(450,500,6));
     fordMustang.changePetrolEngine(new PetrolEngine(110, 1000, 6));
-    std::cout << "- - - - - - \n";
+ 
     ElectricCar nissan(new ElectricEngine(120, 4000));
     nissan.changeElectricEngine(new ElectricEngine(130, 5000));
+  
+    HybridCar rav4(new PetrolEngine(180, 1800,6), new ElectricEngine(120, 5000));
+    rav4.changePetrolEngine(new PetrolEngine(110, 1000, 6));
+    rav4.changeElectricEngine(new ElectricEngine(130, 5000));
+    
+    rav4.accelerate(60);
+    rav4.changePetrolEngine(new PetrolEngine(180, 1800, 6));
+    rav4.changeElectricEngine(new ElectricEngine(140, 5500));
 
-    // HybridCar rav4(new PetrolEngine(180, 1800,6), new ElectricEngine(120, 5000));
     // rav4.accelerate(200);
     // rav4.accelerate(50);
     // rav4.accelerate(50);
