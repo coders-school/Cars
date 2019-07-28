38c4cd31e455c9ad2a48e3e429d6c6591400ec36
Bodzio <bodziohnat@gmail.com>
2019-07-28 13:14:43 +0200

    velocity1

--- Car.cpp
+++ Car.cpp
@@ -5,3 +5,5 @@ void Car::turnLeft()      { std::cout << __FUNCTION__ << std::endl; }
 void Car::turnRight()     { std::cout << __FUNCTION__ << std::endl; }
 void Car::brake()         { std::cout << __FUNCTION__ << std::endl; }
 void Car::accelerate(int) { std::cout << __FUNCTION__ << std::endl; }
+void Car::velocity()      { std::cout << __FUNCTION__ << std::endl; }
+
--- Car.hpp
+++ Car.hpp
@@ -6,6 +6,7 @@ public:
     virtual ~Car() = default;
     void turnLeft();
     void turnRight();
+    void velocity();
     void brake();
     void accelerate(int speed);
     virtual void feed()=0;
