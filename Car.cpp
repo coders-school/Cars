cba7ad87a874d08fc80718277b0693c3e0b822de
Bodzio <bodziohnat@gmail.com>
2019-07-31 21:34:04 +0200

    complete velo

--- Car.cpp
+++ Car.cpp
@@ -3,8 +3,7 @@
 
 void Car::turnLeft()         { std::cout << __FUNCTION__ << std::endl; }
 void Car::turnRight()  	     { std::cout << __FUNCTION__ << std::endl; }
-void Car::brake()            { std::cout << __FUNCTION__ << std::endl; }
-			     velocity = 0; 
+void Car::brake()            { std::cout << __FUNCTION__ << std::endl; 
+			      velocity = 0;} 
 void Car::accelerate(int)    { std::cout << __FUNCTION__ << std::endl; }
-void Car::velocity()         { std::cout << __FUNCTION__ << std::endl; }
 
