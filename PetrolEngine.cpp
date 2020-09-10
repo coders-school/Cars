fa3288b01890082ba5613cade0425525656cbca0
m-kop <m.kopertowski@epicspace.eu>
2020-09-10 12:19:01 +0200

    add InvalidGear logic

--- PetrolEngine.cpp
+++ PetrolEngine.cpp
@@ -12,10 +12,11 @@ PetrolEngine::PetrolEngine(int power, float capacity, int gears)
 
 void PetrolEngine::changeGear(int gear)
 {
-    // TODO: Add checking if gear is between -1 and gears_
-    // -1 is for REAR
-    // 0 is for NEUTRAL
     if ((gear < -1) or (gear > gears_)) {
+        throw InvalidGear{};
+    } else if ((currentGear_ > 0) and (gear == -1)) {
+        throw InvalidGear{};
+    } else if ((currentGear_ == -1) and (gear > 0)) {
         throw InvalidGear{};
     }
 
--- PetrolEngineTest.cpp
+++ PetrolEngineTest.cpp
@@ -9,8 +9,20 @@ struct PetrolEngineTest : public ::testing::Test {
 TEST_F(PetrolEngineTest, shouldThrowException) {
     //WHEN
     EXPECT_THROW(engine.changeGear(120), InvalidGear);
+    EXPECT_THROW(engine.changeGear(-5), InvalidGear);
+    engine.changeGear(-1);
+    EXPECT_THROW(engine.changeGear(1), InvalidGear);
+    engine.changeGear(0);
+    engine.changeGear(2);
+    EXPECT_THROW(engine.changeGear(-1), InvalidGear);
 }
 TEST_F(PetrolEngineTest, shouldNotThrowException) {
     //WHEN
     EXPECT_NO_THROW(engine.changeGear(1));
+    EXPECT_NO_THROW(engine.changeGear(0));
+    EXPECT_NO_THROW(engine.changeGear(-1));
+    EXPECT_NO_THROW(engine.changeGear(0));
+    EXPECT_NO_THROW(engine.changeGear(1));
+    EXPECT_NO_THROW(engine.changeGear(2));
+    EXPECT_NO_THROW(engine.changeGear(4));
 }
