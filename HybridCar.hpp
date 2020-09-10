178a3e1af2a9db68e2195e9a8e9bbfc893067a1f
WojtekMs <wojtek.atmo@gmail.com>
2020-09-10 15:24:17 +0200

    inheritance added hybrid car

--- HybridCar.hpp
+++ HybridCar.hpp
@@ -1,19 +1,16 @@
-#include "ElectricEngine.hpp"
-#include "PetrolEngine.hpp"
+#pragma once
+#include "PetrolCar.hpp"
+#include "ElectricCar.hpp"
 
-class HybridCar
-{
-public:
+class HybridCar :public PetrolCar, public ElectricCar {
+   public:
     HybridCar(PetrolEngine* petrolEng, ElectricEngine* electricEng);
     ~HybridCar();
-    void turnLeft();
-    void turnRight();
-    void brake();
-    void accelerate(int speed);
-    void charge();
-    void refuel();
+    void turnLeft() override;
+    void turnRight() override;
+    void brake() override;
+    void accelerate(int speed) override;
 
     PetrolEngine* petrolEngine_;
     ElectricEngine* electricEngine_;
 };
-
