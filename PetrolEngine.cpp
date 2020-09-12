bcb071c424060e16a33d9753bfb02addb5b054fe
MonikaZ <monika.zelechowska@o2.pl>
2020-09-12 09:53:20 +0200

    Changing InvalidGear exception

--- PetrolEngine.cpp
+++ PetrolEngine.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 #include <stdexcept>
 
+class InvalidGearError : public std::logic_error {
+public:
+    InvalidGearError(std::string gear)
+        : std::logic_error("Invalid Gear - REAR gear cannot be set from " + gear + " gear.") {}
+};
+
 PetrolEngine::PetrolEngine(int power, float capacity, int gears)
     : power_(power), capacity_(capacity), gears_(gears), currentGear_(0) {
     std::cout << __FUNCTION__ << std::endl;
@@ -13,14 +19,15 @@ void PetrolEngine::changeGear(int newGear) {
     if (newGear < gears_ && newGear >= -1) {
         try {
             if (newGear == -1 && currentGear_ != 0) {
-                throw std::out_of_range("Invalid Gear - gear -1 can be set only when current gear is equal to 0.");
+                //throw std::out_of_range("Invalid Gear - gear -1 can be set only when current gear is equal to 0.");
+                throw InvalidGearError{std::to_string(currentGear_)};
             }
 
             currentGear_ = newGear;
             std::cout << __FUNCTION__ << std::endl;
             std::cout << "Gear was changed to: " << currentGear_ << std::endl;
-        } catch (const std::exception& exeption) {
-            std::cerr << exeption.what() << '\n';
+        } catch (InvalidGearError& currentGear_) {
+            std::cerr << currentGear_.what() << "\n";
         }
     }
 }
