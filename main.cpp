50891e867ff7e1c754de4f9819829e9c2bb65ec0
WojtekMs <wojtek.atmo@gmail.com>
2020-09-15 18:03:54 +0200

    example of interface usage

--- main.cpp
+++ main.cpp
@@ -4,24 +4,15 @@
 #include "PetrolCar.hpp"
 
 int main() {
-    std::cout << std::endl << "OPEL" << std::endl;
-    PetrolCar opel(new PetrolEngine(120, 1800, 6));
-    opel.accelerate(50);
-    opel.brake();
-    opel.accelerate(-900);
-    opel.refuel();
-
-    std::cout << std::endl << "NISSAN" << std::endl;
-    ElectricCar nissan(new ElectricEngine(130, 650));
-    nissan.charge();
-    nissan.accelerate(80);
-    nissan.engine_ = new ElectricEngine(150, 700);  // Changing an engine during driving is not safe
-    nissan.turnLeft();
-
-    std::cout << std::endl << "TOYOTA" << std::endl;
-    HybridCar toyota(new PetrolEngine(80, 1400, 5), new ElectricEngine(100, 540));
-    toyota.accelerate(100);
-    toyota.brake();
-    toyota.charge();
-    toyota.refuel();
+    PetrolEngine defaultEngine = PetrolEngine(HorsePower(100), CubicCentimetre(1500), Gear(5));
+    Vehicle* veh = new PetrolCar(new PetrolEngine{defaultEngine}, Litre(60));
+    veh->turnLeft();
+    
+    std::cout << std::endl
+              << "OPEL" << std::endl;
+    PetrolCar opel(new PetrolEngine{defaultEngine}, Litre(50));
+    delete veh;
+    veh = &opel;
+    veh->changeSpeedTo(KMH(100));
+    veh->restore();
 }
