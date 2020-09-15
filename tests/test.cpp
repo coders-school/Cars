e24b41d2b237a7a413c2b93a2b97d9e1399c7f7c
WojtekMs <wojtek.atmo@gmail.com>
2020-09-15 17:35:14 +0200

    added vehicle tests

---
+++ tests/test.cpp
@@ -0,0 +1,49 @@
+#include <gtest/gtest.h>
+#include <unordered_map>
+
+#include "PetrolCar.hpp"
+#include "Vehicle.hpp"
+
+class VehicleTests : public ::testing::Test {
+   public:
+    PetrolCar car{new PetrolEngine(HorsePower(100), CubicCentimetre(1000), Gear(4))};
+};
+
+const std::unordered_map<Direction, std::string> directionToString {
+    {Direction::LEFT, "LEFT"},
+    {Direction::DOWN, "DOWN"},
+    {Direction::UP, "UP"},
+    {Direction::RIGHT, "RIGHT"}
+};
+
+std::ostream& operator<<(std::ostream& os, Direction dir) {
+    os << directionToString.at(dir);
+    return os;
+}
+
+TEST_F(VehicleTests, turnLeftShouldChangeDirection) {
+    std::unordered_map<Direction, Direction> newDirectionAfterTurnLeft{
+        {Direction::LEFT, Direction::DOWN},
+        {Direction::UP, Direction::LEFT},
+        {Direction::DOWN, Direction::RIGHT},
+        {Direction::RIGHT, Direction::UP}};
+    for (int i = 0; i < 100; ++i) {
+        auto expectedDirection = newDirectionAfterTurnLeft.at(car.getCurrentDirection());
+        car.turnLeft();
+        EXPECT_EQ(expectedDirection, car.getCurrentDirection());
+    }
+}
+
+TEST_F(VehicleTests, turnRightShouldChangeDirection) {
+    std::unordered_map<Direction, Direction> newDirectionAfterTurnRight{
+        {Direction::LEFT, Direction::UP},
+        {Direction::UP, Direction::RIGHT},
+        {Direction::DOWN, Direction::LEFT},
+        {Direction::RIGHT, Direction::DOWN}};
+    for (int i = 0; i < 100; ++i) {
+        auto expectedDirection = newDirectionAfterTurnRight.at(car.getCurrentDirection());
+        car.turnRight();
+        EXPECT_EQ(expectedDirection, car.getCurrentDirection());
+    }
+}
+
