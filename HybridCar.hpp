9f04936bd78c89c9bd6463d77eabb7a38a6c8698
Dawid <dawid.d.jozefczak@gmail.com>
2020-10-07 20:11:08 +0200

    Modifications in the lesson

--- HybridCar.hpp
+++ HybridCar.hpp
@@ -1,19 +1,13 @@
+#pragma once
+
+#include "ElectricCar.hpp"
 #include "ElectricEngine.hpp"
+#include "PetrolCar.hpp"
 #include "PetrolEngine.hpp"
 
-class HybridCar
-{
+class HybridCar : public PetrolCar, public ElectricCar {
 public:
     HybridCar(PetrolEngine* petrolEng, ElectricEngine* electricEng);
     ~HybridCar();
-    void turnLeft();
-    void turnRight();
-    void brake();
-    void accelerate(int speed);
-    void charge();
-    void refuel();
-
-    PetrolEngine* petrolEngine_;
-    ElectricEngine* electricEngine_;
+    void restore() override;
 };
-
