fc29dd3259fd3a14550f626ec6683b200a84a0e5
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-10-10 14:09:02 +0100

    Replace new operator with std::make_shared; add exception handling

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 void testPetrolCar(Car* car) {
     std::cout << std::endl << "# PETROL CAR TEST #" << std::endl;
     
-    PetrolCar petrol(new PetrolEngine(120, 1800, 5));
+    PetrolCar petrol(std::make_shared<PetrolEngine>(120, 1800, 5));
     petrol.accelerateTo(50);
     try {
         petrol.accelerateTo(-900);
@@ -15,9 +15,12 @@ void testPetrolCar(Car* car) {
         std::cerr << " : " << ia.what() << std::endl;
     }
     petrol.brake();
-    petrol.setEngine(new PetrolEngine(160, 2000, 6));
-    petrol.restore();
-
+    try {
+        petrol.setEngine(std::make_shared<PetrolEngine>(160, 2000, 6));
+        petrol.restore();
+    } catch (const std::invalid_argument& ia) {
+        std::cerr << " : " << ia.what() << std::endl;
+    }
     car = &petrol;
     car->accelerateTo(100);
     car->changeGear(4);
@@ -29,40 +32,74 @@ void testPetrolCar(Car* car) {
     } catch (const std::invalid_argument& ia) {
         std::cerr << " : " << ia.what() << std::endl;
     }
-    car->restore();
+    try {
+        car->restore();
+    } catch (const std::invalid_argument& ia) {
+        std::cerr << " : " << ia.what() << std::endl;
+    }
 }
 
 void testElectricCar(Car* car) {
     std::cout << std::endl << "# ELECTRIC CAR TEST #" << std::endl;
 
-    ElectricCar electric(new ElectricEngine(130, 650));
-    electric.restore();
+    ElectricCar electric(std::make_shared<ElectricEngine>(130, 650));
+    try {
+        electric.restore();
+    } catch (const std::invalid_argument& ia) {
+        std::cerr << " : " << ia.what() << std::endl;
+    }
     electric.accelerateTo(80);
-    electric.setEngine(new ElectricEngine(210, 1100));
+    // electric.brake(); // to avoid memory leak
+    try {
+        electric.setEngine(std::make_shared<ElectricEngine>(210, 1100));
+    } catch (const std::invalid_argument& ia) {
+        std::cerr << " : " << ia.what() << std::endl;
+    }
     electric.turnLeft();
 
     car = &electric;
     car->changeGear(REVERSE);
-    car->restore();
+    try {
+        car->restore();
+    } catch (const std::invalid_argument& ia) {
+        std::cerr << " : " << ia.what() << std::endl;
+    }
 }
 
 void testHybridCar(Car* car) {
     std::cout << std::endl << "# HYBRID CAR TEST #" << std::endl;
 
-    HybridCar hybrid(new PetrolEngine(80, 1400, 5), new ElectricEngine(100, 540));
+    HybridCar hybrid(std::make_shared<PetrolEngine>(80, 1400, 5),
+                     std::make_shared<ElectricEngine>(100, 540));
     hybrid.accelerateTo(100);
-    hybrid.setEngine(new PetrolEngine(145, 190, 6), new ElectricEngine(130, 660));
+    try {
+        hybrid.setEngine(std::make_shared<PetrolEngine>(145, 190, 6));
+    } catch (const std::invalid_argument& ia) {
+        std::cerr << " : " << ia.what() << std::endl;
+    }
     hybrid.brake();
-    hybrid.restore();
-
+    try {
+        hybrid.setEngine(std::make_shared<ElectricEngine>(130, 660));
+    } catch (const std::invalid_argument& ia) {
+        std::cerr << " : " << ia.what() << std::endl;
+    }
+    try {
+        hybrid.restore();
+    } catch (const std::invalid_argument& ia) {
+        std::cerr << " : " << ia.what() << std::endl;
+    }
     car = &hybrid;
-    car->setEngine(new PetrolEngine(120, 160, 5), new ElectricEngine(100, 770));
     car->changeGear(9);
-    car->restore();
+    try {
+        car->restore();
+    } catch (const std::invalid_argument& ia) {
+        std::cerr << " : " << ia.what() << std::endl;
+    }
 }
 
 int main() {
     Car* car = nullptr;
+
     testPetrolCar(car);
     testElectricCar(car);
     testHybridCar(car);
