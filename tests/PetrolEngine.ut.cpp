6be69da24d355bccb46172b201ef3201918b2d13
Vicx95 <wiktor.zyla31@gmail.com>
2020-10-31 16:45:52 +0100

    Extract test fixture PetrolEngine

--- tests/PetrolEngine.ut.cpp
+++ tests/PetrolEngine.ut.cpp
@@ -7,25 +7,34 @@ constexpr int thirdGear = 3;
 constexpr int fourthGear = 4;
 constexpr int tooHighGear = 9;
 
-TEST(PetrolEngineTest, CallingChangeGearShouldChangeGear){
-    PetrolEngine engine(150, 300, 5);
+constexpr int basicPower = 150;
+constexpr int basicCapacity = 300;
+constexpr int defaultNumberOfGears = 5;
+
+class PetrolEngineTest : public ::testing::Test {
+public:
+    PetrolEngineTest()
+        : engine(basicPower, basicCapacity, defaultNumberOfGears){}
+
+    PetrolEngine engine;
+};
+
+
+TEST_F(PetrolEngineTest, CallingChangeGearShouldChangeGear){
     engine.changeGear(thirdGear);
     EXPECT_EQ(thirdGear, engine.getCurrentGear());
 }
 
-TEST(PetrolEngineTest, PassingReverseGearWhenHavingPositiveShouldThrowException){
-    PetrolEngine engine(150, 300, 5);
+TEST_F(PetrolEngineTest, PassingReverseGearWhenHavingPositiveShouldThrowException){
     engine.changeGear(thirdGear);
     EXPECT_THROW(engine.changeGear(reverseGear), InvalidGearException);
 }
 
-TEST(PetrolEngineTest, PassingToHighGearShouldThrowException){
-    PetrolEngine engine(150, 300, 5);
+TEST_F(PetrolEngineTest, PassingToHighGearShouldThrowException){
     EXPECT_THROW(engine.changeGear(tooHighGear), InvalidGearException);
 }
 
-TEST(PetrolEngineTest, PassingReverseGearDuringHavingNeutralShouldWork){
-    PetrolEngine engine(150, 300, 5);
+TEST_F(PetrolEngineTest, PassingReverseGearDuringHavingNeutralShouldWork){
     engine.changeGear(reverseGear);
     EXPECT_EQ(reverseGear, engine.getCurrentGear());
 }
