43b20787c454350076e65550d5f940863781c0b8
duszek <marcin.dus@gmail.com>
2020-11-02 23:12:27 +0100

    fix after review - added tests for hybrid car, to be continued...

--- test.cpp
+++ test.cpp
@@ -8,9 +8,9 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-class CarTestSuite : public ::testing::Test {
+class PetrolCarTestSuite : public ::testing::Test {
 public:
-  CarTestSuite()
+  PetrolCarTestSuite()
       : m_engineMock_ptr(new EngineMock),
         m_engineMock_rawPtr(m_engineMock_ptr.get()),
         m_sut(std::move(m_engineMock_ptr)) {}
@@ -20,12 +20,30 @@ public:
   PetrolCar m_sut;
 };
 
-TEST_F(CarTestSuite, stopEngineShouldCallStopOEngine) {
+
+class HybridCarTestSuite : public ::testing::Test {
+public:
+  HybridCarTestSuite()
+      : m_engineMock_petrol_ptr(new EngineMock),
+        m_engineMock_electric_ptr(new EngineMock),
+        m_engineMock_petrol_rawPtr(m_engineMock_petrol_ptr.get()),
+        m_engineMock_electric_rawPtr(m_engineMock_electric_ptr.get()),
+        m_sut(std::move(m_engineMock_petrol_ptr),std::move(m_engineMock_electric_ptr)) {}
+
+  std::unique_ptr<EngineMock> m_engineMock_petrol_ptr = nullptr;
+  std::unique_ptr<EngineMock> m_engineMock_electric_ptr = nullptr;
+  EngineMock *m_engineMock_petrol_rawPtr = nullptr;
+  EngineMock *m_engineMock_electric_rawPtr = nullptr;
+  HybridCar m_sut;
+};
+
+
+TEST_F(PetrolCarTestSuite, stopEngineShouldCallStopOEngine) {
   EXPECT_CALL(*m_engineMock_rawPtr, stop());
   m_sut.stop_engine();
 }
 
-TEST_F(CarTestSuite, startEngineShouldCallStopOEngine) {
+TEST_F(PetrolCarTestSuite, startEngineShouldCallStopOEngine) {
   EXPECT_CALL(*m_engineMock_rawPtr, start());
   m_sut.start_engine();
 }
@@ -42,6 +60,18 @@ TEST(EngineTestSuite, ProperGear) {
   engine.changeGear(0);
   engine.changeGear(1);
   EXPECT_EQ(engine.currentGear_, 1);
+}
+
+TEST_F(HybridCarTestSuite, stopEngineShouldCallStopOEngine) {
+  EXPECT_CALL(*m_engineMock_petrol_rawPtr, stop());
+  EXPECT_CALL(*m_engineMock_electric_rawPtr, stop());
+  m_sut.stop_engine();
+}
+
+TEST_F(HybridCarTestSuite, startEngineShouldCallStopOEngine) {
+  EXPECT_CALL(*m_engineMock_petrol_rawPtr, start());
+  EXPECT_CALL(*m_engineMock_electric_rawPtr, start());
+  m_sut.start_engine();
 }
 
 
