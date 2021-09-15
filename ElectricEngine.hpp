c21ec406a13511eb0b6066da48d7df19a284dc64
Wojtek Płóciennik <wojtek.plociennik@gmail.com>
2021-09-15 21:41:53 +0200

    Correcting encapsualtion in ElectricEngine class

--- ElectricEngine.hpp
+++ ElectricEngine.hpp
@@ -4,7 +4,13 @@ class ElectricEngine
 {
 public:
     ElectricEngine(int power, int batteryCapacity);
+    int getPower() const;
+    int getBatteryCapacity() const;
 
+    void setPower();
+    void setBaterryCapacity();
+    
+private:
     int power_;             // in HP 
     int batteryCapacity_;   // in Ah
 };
