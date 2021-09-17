0b14fea2f2ee3775e63779345ac7a4b693c1f2d8
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-09-17 11:15:48 +0200

    Add inheritance from abstract Engine class & move 'power' data member there

---
+++ engine/PetrolEngine.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Engine.hpp"
+
+class PetrolEngine : public Engine
+{
+public:
+    PetrolEngine(int power, float capacity, int gears);
+    void changeGear(int gear);
+
+private:
+    float capacity_;    // in ccm
+    int gears_;
+    int currentGear_;
+};
