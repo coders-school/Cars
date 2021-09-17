c9bdc31c08d679f1f9bc8ad2e8a1327b437c7d4c
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-09-17 11:10:31 +0200

    Remove files to appropriate directories, i.e. car & engine

--- ElectricEngine.cpp
+++
@@ -1,9 +0,0 @@
-#include "ElectricEngine.hpp"
-#include <iostream>
-
-ElectricEngine::ElectricEngine(int power, int batteryCapacity)
-    : power_(power)
-    , batteryCapacity_(batteryCapacity)
-{
-    std::cout << __FUNCTION__ << std::endl;
-}
