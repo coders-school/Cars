ad646e2724487902e370f6fda8b2cfb836957de8
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-09-18 17:25:06 +0200

    Add testing gear change by catching an exception; add car names

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 
 #include "car/Car.hpp"
 #include "car/PetrolCar.hpp"
@@ -10,56 +11,43 @@
 
 int main()
 {
-    Car* car;
+    Car* car{};
 
-    car = new PetrolCar(new PetrolEngine(120, 1800, 6));
+    auto isuzu = new PetrolCar(new PetrolEngine(120, 1800, 6));
+    car = isuzu;
     car->accelerate(90);
     car->turnLeft();
     car->turnRight();
     car->brake();
     car->restore();
-    delete car;
+    try {
+        isuzu->changeGear(-2);
+    } catch (std::logic_error& le) {
+        std::cout << le.what() << std::endl;
+    }
+    delete isuzu;
     std::cout << "-----------------------" << std::endl;
 
-    car = new ElectricCar(new ElectricEngine(130, 650));
+    auto nissan = new ElectricCar(new ElectricEngine(130, 650));
+    car = nissan;
     car->accelerate(90);
     car->turnLeft();
     car->turnRight();
     car->brake();
     car->restore();
-    delete car;
+    delete nissan;
     std::cout << "-----------------------" << std::endl;
 
-    car = new HybridCar(new PetrolEngine(90, 1400, 4),
-                        new ElectricEngine(80, 420));
+    auto toyota = new HybridCar(new PetrolEngine(90, 1400, 4),
+                                new ElectricEngine(80, 420));
+    car = toyota;
     car->accelerate(90);
     car->turnLeft();
     car->turnRight();
     car->brake();
     car->restore();
-    delete car;
+    delete toyota;
     std::cout << "-----------------------" << std::endl;
-    
 
-    // car = new ElectricCar(engine);
-    // std::cout << std::endl << "OPEL" << std::endl;
-    // PetrolCar opel(new PetrolEngine(120, 1800, 6));
-    // opel.accelerate(50);
-    // opel.brake();
-    // opel.accelerate(-900);
-    // opel.refuel();
-
-    // std::cout << std::endl << "NISSAN" << std::endl;
-    // ElectricCar nissan(new ElectricEngine(130, 650));
-    // nissan.charge();
-    // nissan.accelerate(80);
-    // nissan.engine_ = new ElectricEngine(150, 700);  // Changing an engine during driving is not safe
-    // nissan.turnLeft();
-
-    // std::cout << std::endl << "TOYOTA" << std::endl;
-    // HybridCar toyota(new PetrolEngine(80, 1400, 5), new ElectricEngine(100, 540));
-    // toyota.accelerate(100);
-    // toyota.brake();
-    // toyota.charge();
-    // toyota.refuel();
+    car = nullptr;
 }
