10d3ba90a7931fd960d834bb14484d3e45a52766
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-09-20 09:34:24 +0200

    Check battery capacity in c'tor to avoid passing negative one

--- engine/ElectricEngine.cpp
+++ engine/ElectricEngine.cpp
@@ -2,9 +2,15 @@
 
 #include <iostream>
 
+#include "exception/InvalidParameter.hpp"
+
 ElectricEngine::ElectricEngine(int power, int batteryCapacity)
     : Engine(power)
-    , batteryCapacity_(batteryCapacity)
 {
     std::cout << __FUNCTION__ << std::endl;
+    if (batteryCapacity <= 0) {
+        throw InvalidParameter("> battery capacity cannot be negative!");
+    }
+
+    batteryCapacity_ = batteryCapacity;
 }
