dad6607d917d4c9d1c7e18678c251e2c1e308163
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-09-20 09:32:30 +0200

    Add test suite for electric car

---
+++ test/ElectricCar-ut.cpp
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+#include <memory>
+#include <stdexcept>
+
+#include "car/ElectricCar.hpp"
+#include "engine/ElectricEngine.hpp"
+#include "exception/InvalidEngineChange.hpp"
+#include "exception/InvalidSpeed.hpp"
+#include "exception/InvalidParameter.hpp"
+
+struct ElectricCarTest : ::testing::Test {
+    int power{ 110 };
+    float battery_capacity{ 700 };
+    ElectricCar electricCar{ std::make_unique<ElectricEngine>(power, battery_capacity) };
+};
+
+TEST_F(ElectricCarTest, ShouldAccelerateProperly) {
+    electricCar.accelerate(42);
+    EXPECT_EQ(electricCar.getSpeed(), 42);    
+}
+
+TEST_F(ElectricCarTest, ShoulNotAccelerateButThrowException) {
+    EXPECT_THROW(electricCar.accelerate(-42), InvalidSpeed);    
+}
+
+TEST_F(ElectricCarTest, ShouldBrake) {
+    electricCar.brake();
+    EXPECT_EQ(electricCar.getSpeed(), 0);
+}
+
+TEST_F(ElectricCarTest, EngineShouldBeChangedWhenStopped) {
+    EXPECT_NO_THROW(electricCar.changeEngine(std::make_unique<ElectricEngine>(power, battery_capacity)));
+}
+
+TEST_F(ElectricCarTest, EngineNotShouldBeChangedWhileDriving) {
+    electricCar.accelerate(42);
+    EXPECT_THROW(electricCar.changeEngine(std::make_unique<ElectricEngine>(power, battery_capacity)),
+                 InvalidEngineChange);
+}
+
+TEST_F(ElectricCarTest, EngingeCannotBeInitializedWithNegativeCapacity) {
+    battery_capacity = -700;
+    EXPECT_THROW(electricCar.changeEngine(std::make_unique<ElectricEngine>(power, battery_capacity)),
+                 InvalidParameter);
+}
+
+TEST_F(ElectricCarTest, EngingeCannotBeInitializedWithNegativePower) {
+    power = -110;
+    EXPECT_THROW(electricCar.changeEngine(std::make_unique<ElectricEngine>(power, battery_capacity)),
+                 InvalidParameter);
+}
