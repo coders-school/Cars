bb04cbccb6b9f73f6d13923718d313dab7d3f84b
Wojtek Płóciennik <wojtek.plociennik@gmail.com>
2021-09-20 18:59:29 +0200

    add Car class

---
+++ Car.hpp
@@ -0,0 +1,13 @@
+#pragma once
+class Car {
+public:
+    void turnLeft();
+    void turnRight();
+    void brake();
+    void accelerate(int speed);
+    virtual void refill() = 0;
+    virtual ~Car(){};
+private:
+    double speed_{0.0};
+   
+};
