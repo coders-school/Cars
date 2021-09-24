0a78ae1a6d7789a01e45983ebe9f2c9c7e223909
lukasz-kukulka <qqla83@gmail.com>
2021-09-24 22:29:00 +0100

    After fix bug 4

--- main.cpp
+++ main.cpp
@@ -10,54 +10,49 @@
 
 int main()
 {
-    Car* car;
-    Engine* electricEngine;
-    Engine* petrolEngine;
-    PetrolEngine petrolEng (80, 1400, 5);
-    petrolEngine = &petrolEng;
+    // Car* car;
+    // Engine* electricEngine;
+    // Engine* petrolEngine;
+    // PetrolEngine petrolEng (80, 1400, 5);
+    // petrolEngine = &petrolEng;
 
-    std::cout << std::endl << "OPEL" << std::endl;
-    PetrolCar opel(petrolEngine);
-    car = &opel;
-    try {
-        car->accelerate(50);
-    } catch (const Car::InvalidAccelerate & error){
-        std::cout << error.what() << '\n';
-    }
-    car->brake();
-    try {
-        car->accelerate(-900);
-    } catch (const Car::InvalidAccelerate & error){
-        std::cout << error.what() << '\n';
-    }
-    car->fillUp();
-    try {
-        opel.changeGear(1, petrolEng);
-    } catch (const PetrolEngine::InvalidGear & error){
-        std::cout << error.what() << '\n';
-    }
+    // std::cout << std::endl << "OPEL" << std::endl;
+    // PetrolCar opel(petrolEngine);
+    // car = &opel;
+    // try {
+    //     car->accelerate(50);
+    // } catch (const Car::InvalidAccelerate & error){
+    //     std::cout << error.what() << '\n';
+    // }
+    // car->brake();
+    // try {
+    //     car->accelerate(-900);
+    // } catch (const Car::InvalidAccelerate & error){
+    //     std::cout << error.what() << '\n';
+    // }
+    // car->fillUp();
+    // opel.changeGear(1, petrolEng);
+    // std::cout << std::endl << "NISSAN" << std::endl;
+    // ElectricEngine electricEng(100, 1110);
+    // electricEngine = &electricEng;
+    // ElectricCar nissan(electricEngine);
+    // car = &nissan;
+    // car->fillUp();
+    // try {
+    //     car->accelerate(80);
+    // } catch (const Car::InvalidAccelerate & error){
+    //     std::cout << error.what() << '\n';
+    // }
+    // ElectricEngine newElectricEngine(150, 700);
+    // car->turnLeft();
 
-    std::cout << std::endl << "NISSAN" << std::endl;
-    ElectricEngine electricEng(100, 1110);
-    electricEngine = &electricEng;
-    ElectricCar nissan(electricEngine);
-    car = &nissan;
-    car->fillUp();
-    try {
-        car->accelerate(80);
-    } catch (const Car::InvalidAccelerate & error){
-        std::cout << error.what() << '\n';
-    }
-    ElectricEngine newElectricEngine(150, 700);
-    car->turnLeft();
-
-    std::cout << std::endl << "TOYOTA" << std::endl;
-    HybridCar toyota(electricEngine, petrolEngine);
-    try {
-        car->accelerate(-20);
-    } catch (const Car::InvalidAccelerate & error){
-        std::cout << error.what() << '\n';
-    }
-    car->brake();
-    car->fillUp();
+    // std::cout << std::endl << "TOYOTA" << std::endl;
+    // HybridCar toyota(electricEngine, petrolEngine);
+    // try {
+    //     car->accelerate(-20);
+    // } catch (const Car::InvalidAccelerate & error){
+    //     std::cout << error.what() << '\n';
+    // }
+    // car->brake();
+    // car->fillUp();
 }
