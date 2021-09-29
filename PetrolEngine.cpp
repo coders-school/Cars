83ca060d9697af1b90c2cb1da5b28fc234594760
Kacu <kacper.kaleta@scythe-studio.com>
2021-09-29 23:50:25 +0200

    created InvalidGear class. Implemented changeGear

--- PetrolEngine.cpp
+++ PetrolEngine.cpp
@@ -1,4 +1,6 @@
 #include "PetrolEngine.hpp"
+
+#include <cmath>
 #include <iostream>
 
 PetrolEngine::PetrolEngine(int power, float capacity, int gears)
@@ -12,9 +14,15 @@ PetrolEngine::PetrolEngine(int power, float capacity, int gears)
 
 void PetrolEngine::changeGear(int gear)
 {
-    // TODO: Add checking if gear is between -1 and gears_
-    // -1 is for REAR
-    // 0 is for NEUTRAL
+    if (gear < -1) {
+        throw InvalidGear("gear to low");
+    } 
+    if (gear > gears_) {
+        throw InvalidGear("gear to high");  
+    } 
+    if (std::abs(currentGear_ - gear) != 1) {
+        throw InvalidGear("too fast gear shift");
+    } 
     currentGear_ = gear;
     std::cout << __FUNCTION__ << std::endl;
 }
--- PetrolEngine.hpp
+++ PetrolEngine.hpp
@@ -1,8 +1,17 @@
 #pragma once
 
+#include <stdexcept>
+
+
 class PetrolEngine
 {
 public:
+    struct InvalidGear : public std::logic_error {
+        InvalidGear(std::string msg): 
+            std::logic_error(msg) 
+        {}
+    };
+
     PetrolEngine(int power, float capacity, int gears);
     void changeGear(int gear);
 
