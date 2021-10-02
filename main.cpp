bfb4431501adc77b19aa589371c7b69bd6a0c9ea
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-10-02 17:32:42 +0200

    some small changes in main.cpp

--- main.cpp
+++ main.cpp
@@ -5,21 +5,32 @@
 
 int main()
 {
+    Car* car;
     std::cout << std::endl << "OPEL" << std::endl;
     PetrolCar opel(new PetrolEngine(120, 1800, 6));
-    opel.accelerate(50);
-    opel.brake();
-    opel.accelerate(-900);
-    // opel.refuel();
-    opel.refill();
+    // opel.accelerate(50);
+    // opel.brake();
+    // opel.accelerate(-900);
+    // opel.refill();
+    car = &opel;
+    car->accelerate(50);
+    car->brake();
+    car->accelerate(-900);
+    car->refill();
 
     std::cout << std::endl << "NISSAN" << std::endl;
     ElectricCar nissan(new ElectricEngine(130, 650));
-    // nissan.charge();
+    // nissan.refill();
+    // nissan.accelerate(80);
+    // // nissan.engine_ = new ElectricEngine(150, 700);  // Changing an engine during driving is not safe
+    // nissan.turnLeft();
+    car = &nissan;
     nissan.refill();
     nissan.accelerate(80);
-    // nissan.engine_ = new ElectricEngine(150, 700);  // Changing an engine during driving is not safe
     nissan.turnLeft();
+
+
+    
 
     std::cout << std::endl << "TOYOTA" << std::endl;
     HybridCar toyota(new PetrolEngine(80, 1400, 5), new ElectricEngine(100, 540));
