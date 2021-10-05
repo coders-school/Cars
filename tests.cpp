73df4ad6889feb0d3a3bf67570689fef9b3109ef
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-10-06 00:44:16 +0200

    Gtest framework added (empty)

---
+++ tests.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+#include "PetrolCar.hpp"
+#include "Car.hpp"
+
+TEST(AccelerateTest, ShouldSetCorrectSpeed) {
+    PetrolCar cut;
+    auto expected = 3;
+
+    auto result = cut.get
+
+}
+
+TEST(FactorialTest, HandlesZeroInput) {
+
+
+  EXPECT_EQ(Factorial(0), 1);
+}
+
