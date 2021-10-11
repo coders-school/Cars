46bbd29c8e2fd6310aca726e2f2290a6b241e8eb
aniolm <maciej.aniol.programista@gmail.comgit config --global user.name Your>
2021-10-11 22:48:35 +0300

    Car class modified

--- Car.cpp
+++ Car.cpp
@@ -1,7 +1,32 @@
 #include "Car.hpp"
 #include <iostream>
 
-void Car::turnLeft()      { std::cout << __FUNCTION__ << std::endl; }
-void Car::turnRight()     { std::cout << __FUNCTION__ << std::endl; }
-void Car::brake()         { std::cout << __FUNCTION__ << std::endl; }
-void Car::accelerate(int) { std::cout << __FUNCTION__ << std::endl; }
+void Car::turnLeft() {
+    std::cout << __FUNCTION__ << std::endl;
+}
+void Car::turnRight() {
+    std::cout << __FUNCTION__ << std::endl;
+}
+void Car::brake() {
+    speed_ = 0;
+}
+void Car::accelerate(int speed) {
+    if (speedIsValid(speed)) {
+        std::cout << __FUNCTION__ << std::endl;
+        speed_ = speed;
+    } else {
+        throw InvalidSpeed("Speed out of range!!!");
+    }
+}
+
+bool Car::speedIsValid(const int& speed) {
+    return speed >= 0 && speed <= 200;
+}
+
+void Car::changeEngine(std::unique_ptr<PetrolEngine> petrolEngine) {
+    throw WrongEngine("It is not a petrol car!!!");
+}
+
+void Car::changeEngine(std::unique_ptr<ElectricEngine> electricEngine) {
+    throw WrongEngine("It is not an electric car!!!");
+}
--- Car.hpp
+++ Car.hpp
@@ -1,12 +1,41 @@
 #pragma once
 
-class Car
-{
+#include <stdexcept>
+#include <memory>
+#include "ElectricEngine.hpp"
+#include "PetrolEngine.hpp"
+
+class Car {
 public:
+    virtual ~Car(){};
     void turnLeft();
     void turnRight();
     void brake();
     void accelerate(int speed);
-    virtual void refill() = 0;
-    virtual ~Car(){}
+    int getSpeed() const { return speed_; }
+    virtual void reFill() = 0;
+    virtual void changeEngine(std::unique_ptr<PetrolEngine> engine);
+    virtual void changeEngine(std::unique_ptr<ElectricEngine> engine);
+
+    class InvalidSpeed : public std::range_error {
+    public:
+        InvalidSpeed(const std::string& msg)
+            : std::range_error(msg) {}
+    };
+
+    class WrongEngine : public std::invalid_argument {
+    public:
+        WrongEngine(const std::string& msg)
+            : std::invalid_argument(msg) {}
+    };
+
+    class CarMoving : public std::logic_error {
+    public:
+        CarMoving(const std::string& msg)
+            : std::logic_error(msg) {}
+    };
+
+private:
+    int speed_{0};
+    bool speedIsValid(const int& speed);
 };
