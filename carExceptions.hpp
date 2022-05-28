15d88303bca101b327cbb62d43e03ef101c8a0ca
rizzet97 <nit.pawel@gmail.com>
2022-05-28 17:03:50 +0200

    Added CarLogicError exception

--- carExceptions.hpp
+++ carExceptions.hpp
@@ -5,3 +5,8 @@ class InvalidGear : public std::logic_error {
 public:
     InvalidGear(const std::string & msg) : std::logic_error(msg) {}
 };
+
+class CarLogicError : public std::logic_error {
+public:
+    CarLogicError(const std::string & msg) : std::logic_error(msg) {}
+};
