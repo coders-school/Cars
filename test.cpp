33fab1388236f2fb71315898fd204c462d216b21
rizzet97 <nit.pawel@gmail.com>
2022-05-30 16:57:56 +0200

    Added unit tests for changeGear() and accelerate()

--- test.cpp
+++ test.cpp
@@ -10,34 +10,43 @@
 
 class GearboxTest : public testing::Test {
 protected:
-// petrol engine
+  PetrolCar car{std::make_unique<PetrolEngine>(PetrolEngine(120, 1800, 6))};
 };
 
 class CarTest : public testing::Test {
 protected:
-// petrol car
-// electric car
-// hybrid car
+  PetrolCar carPetrol{std::make_unique<PetrolEngine>(PetrolEngine(120, 1800, 6))};
+  ElectricCar carElectric{std::make_unique<ElectricEngine>(ElectricEngine(130, 650))};
+  HybridCar carHybrid{std::make_unique<PetrolEngine>(PetrolEngine(80, 1400, 5)), std::make_unique<ElectricEngine>(ElectricEngine(100, 540))};
 };
 
 // Gearbox tests
 TEST_F(GearboxTest, ShouldAllowNormalChange) {
-  EXPECT_EQ(1, 2);
+  EXPECT_NO_THROW(car.changeGear(1));
+  EXPECT_NO_THROW(car.changeGear(2));
+  EXPECT_NO_THROW(car.changeGear(4));
+  EXPECT_NO_THROW(car.changeGear(0));
+  EXPECT_NO_THROW(car.changeGear(-1));
+  EXPECT_NO_THROW(car.changeGear(3));
+  EXPECT_NO_THROW(car.changeGear(5));
 }
 TEST_F(GearboxTest, ShouldThrowIfBadGear) {
-  EXPECT_EQ(1, 2);
+  EXPECT_THROW(car.changeGear(8), InvalidGear);
 }
 TEST_F(GearboxTest, ShouldThrowIfReverseFromHighGear) {
-  EXPECT_EQ(1, 2);
+  EXPECT_NO_THROW(car.changeGear(1));
+  EXPECT_THROW(car.changeGear(-1), InvalidGear);
 }
 
 // Car tests
-TEST_F(CarTest, ShouldAllowEngineChangeToSameType) {
-  EXPECT_EQ(1, 2);
+TEST_F(CarTest, ShouldAllowNormalAcceleration) {
+  EXPECT_NO_THROW(carPetrol.accelerate(11));
+  EXPECT_NO_THROW(carElectric.accelerate(17)); 
+  EXPECT_NO_THROW(carHybrid.accelerate(20));
 }
-TEST_F(CarTest, ShouldThrowWhenEngineChangeToWrongType) {
-  EXPECT_EQ(1, 2);
-}
-TEST_F(CarTest, ShouldThrowWhenEmptyEngineChange) {
-  EXPECT_EQ(1, 2);
+
+TEST_F(CarTest, ShouldThrowWhenNegativeAcceleration) {
+  EXPECT_THROW(carPetrol.accelerate(-50), CarLogicError);
+  EXPECT_THROW(carElectric.accelerate(-50), CarLogicError); 
+  EXPECT_THROW(carHybrid.accelerate(-50), CarLogicError);
 }
