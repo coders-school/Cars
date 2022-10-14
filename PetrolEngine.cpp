2b6900913f94c1feba1a91a5749d8d3fdfda682f
adrian <7adrian.mail@gmail.com>
2022-10-14 16:54:38 +0200

    Adding initial commit for Google test library

--- PetrolEngine.cpp
+++ PetrolEngine.cpp
@@ -1,5 +1,12 @@
 #include "PetrolEngine.hpp"
 #include <iostream>
+#include <stdexcept>
+
+struct InvalidGear : public std::logic_error {
+    [[maybe_unused]] InvalidGear() : std::logic_error("Invalid gear") {}
+
+    [[maybe_unused]] InvalidGear(std::string message) : std::logic_error(message) {}
+};
 
 PetrolEngine::PetrolEngine(int power, float capacity, int gears)
         : Engine(power), capacity_(capacity), gears_(gears), currentGear_(0) {
@@ -7,9 +14,15 @@ PetrolEngine::PetrolEngine(int power, float capacity, int gears)
 }
 
 void PetrolEngine::changeGear(int gear) {
-    // TODO: Add checking if gear is between -1 and gears_
-    // -1 is for REAR
-    // 0 is for NEUTRAL
+    if (gear < -1) {
+        throw InvalidGear("Gear can't be less than -1");
+    }
+    if (gear > gears_) {
+        throw InvalidGear("Gear can't be greater than " + std::to_string(gears_));
+    }
+    if (gear == -1 && currentGear_ > 1) {
+        throw InvalidGear("Can't change gear to reverse from gear greater than 1");
+    }
     currentGear_ = gear;
     std::cout << __FUNCTION__ << std::endl;
 }
